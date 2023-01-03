f520d300414c9f41e80fe4d2546ff35ae446d354
Matheus Honorato <matheuswebmw@gmail.com>
2023-01-02 22:53:41 -0300

    Criado a listagem de usuarios

--- crudjspjava/src/main/java/com/crudjspjava/dao/UsuarioDao.java
+++ crudjspjava/src/main/java/com/crudjspjava/dao/UsuarioDao.java
@@ -2,7 +2,12 @@ package com.crudjspjava.dao;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
 
+import com.crudjspjava.bean.Usuario;
 
 public class UsuarioDao {
 	
@@ -18,5 +23,30 @@ public class UsuarioDao {
 		return con;
 	}
 	
-	
+	public static List<Usuario> getAllUsuarios(){
+		List<Usuario> list = new ArrayList<Usuario>();
+		
+		try {
+			Connection con = getConnection();
+			PreparedStatement ps = (PreparedStatement) con.prepareStatement("SELECT * FROM usuario");
+			
+			ResultSet rs = ps.executeQuery();
+			
+			while(rs.next()) {
+				Usuario usuario = new Usuario();
+				
+				usuario.setId(rs.getInt("id"));
+				usuario.setNome(rs.getString("nome"));
+				usuario.setPassword(rs.getString("password"));
+				usuario.setSexo(rs.getString("sexo"));
+				usuario.setPais(rs.getString("pais"));
+				list.add(usuario);
+			}
+		}catch(Exception e) {
+			System.out.println(e);
+		}
+		
+		return list;
+		
+	}
 }
